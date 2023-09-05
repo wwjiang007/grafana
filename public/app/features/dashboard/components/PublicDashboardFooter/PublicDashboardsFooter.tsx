768457655d86cfbdd315f7201ad3e218dbecd78e
Juan Cabanas <juan.cabanas@grafana.com>
2023-09-05 11:52:42 -0300

    footer modification, based on text and not image

--- public/app/features/dashboard/components/PublicDashboardFooter/PublicDashboardsFooter.tsx
+++ public/app/features/dashboard/components/PublicDashboardFooter/PublicDashboardsFooter.tsx
@@ -6,33 +6,41 @@ import { useStyles2 } from '@grafana/ui';
 
 export interface PublicDashboardFooterCfg {
   hide: boolean;
-  text: string;
+  text: React.ReactNode;
   logo: string;
   link: string;
 }
 
 export const PublicDashboardFooter = function () {
   const styles = useStyles2(getStyles);
-  const conf = getPublicDashboardFooterConfig();
+  const conf = useGetPublicDashboardFooterConfig();
 
   return conf.hide ? null : (
     <div className={styles.footer}>
       <a className={styles.link} href={conf.link} target="_blank" rel="noreferrer noopener">
-        {conf.text} <img className={styles.logoImg} alt="" src={conf.logo}></img>
+        {conf.text} <img className={styles.logoImg} alt="" src={conf.logo} />
       </a>
     </div>
   );
 };
 
-export function setPublicDashboardFooterConfigFn(fn: typeof getPublicDashboardFooterConfig) {
-  getPublicDashboardFooterConfig = fn;
+export function setPublicDashboardFooterConfigFn(fn: typeof useGetPublicDashboardFooterConfig) {
+  useGetPublicDashboardFooterConfig = fn;
 }
-export let getPublicDashboardFooterConfig = (): PublicDashboardFooterCfg => ({
-  hide: false,
-  text: 'powered by Grafana',
-  logo: 'public/img/grafana_icon.svg',
-  link: 'https://grafana.com/',
-});
+export let useGetPublicDashboardFooterConfig = (): PublicDashboardFooterCfg => {
+  const styles = useStyles2(getStyles);
+
+  return {
+    hide: false,
+    text: (
+      <p className={styles.textContainer}>
+        Powered by <img className={styles.logoImg} alt="grafana-logo" src="public/img/grafana_icon.svg" /> Grafana
+      </p>
+    ),
+    logo: '',
+    link: 'https://grafana.com/',
+  };
+};
 
 const getStyles = (theme: GrafanaTheme2) => ({
   footer: css`
@@ -46,6 +54,9 @@ const getStyles = (theme: GrafanaTheme2) => ({
     gap: 4px;
     justify-content: end;
     align-items: center;
+  `,
+  textContainer: css`
+    height: 100%;
   `,
   logoImg: css`
     height: 100%;
