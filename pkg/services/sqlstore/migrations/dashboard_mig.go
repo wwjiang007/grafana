2d234b6ecf0cbba13b02d2a3ec2a3e40050273ed
Sofia Papagiannaki <1632407+papagian@users.noreply.github.com>
2023-09-11 11:08:53 +0300

    Add schema migration for folder UID

--- pkg/services/sqlstore/migrations/dashboard_mig.go
+++ pkg/services/sqlstore/migrations/dashboard_mig.go
@@ -234,4 +234,8 @@ func addDashboardMigration(mg *Migrator) {
 	mg.AddMigration("Add isPublic for dashboard", NewAddColumnMigration(dashboardV2, &Column{
 		Name: "is_public", Type: DB_Bool, Nullable: false, Default: "0",
 	}))
+
+	mg.AddMigration("Add folder_uid for dashboard", NewAddColumnMigration(dashboardV2, &Column{
+		Name: "folder_uid", Type: DB_NVarchar, Length: 40, Nullable: true,
+	}))
 }
